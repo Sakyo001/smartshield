e3153c70067482012aede7febf3342fd21cf103b
Sakyo001 <masagnayjohnroel11@gmail.com>
2025-11-24 15:37:42 +0800

    use v4

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  cacheComponents: true,
+  experimental: {
+    turbo: {
+      root: process.cwd(),
+    },
+  },
 };
 
 export default nextConfig;
