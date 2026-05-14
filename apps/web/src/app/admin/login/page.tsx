d16f6f7314fb8c12f0e92b41452b771a1c38235b
sakyo11 <masagnayjohnroel11@gmail.com>
2026-05-15 00:40:59 +0800

    removed wrong path

--- apps/web/src/app/admin/login/page.tsx
+++ apps/web/src/app/admin/login/page.tsx
@@ -1,5 +1,5 @@
 // src/app/admin/login/page.tsx
-import { AdminLoginForm } from "@components/AdminLoginForm";
+import { AdminLoginForm } from "../../components/AdminLoginForm";
 
 export default function AdminLoginPage() {
   return (
